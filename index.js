eee907cf462651feb5fb266e7a061172ed9860b3
Jose Soto <jsoto7087@gmail.com>
2022-06-21 12:32:40 -0400

    commit tree de6efe9b3e6d3b9a6e5b548c2d88b9ad4003d6f3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function createEmployeeRecord(array) {
+    const employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+    return employee;
+
+
+}
+
+
+
+function createEmployeeRecords(array) {
+    return array.map(function (row) {
+        return createEmployeeRecord(row);
+    })
+
+}
